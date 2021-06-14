4c26be99e5ff9281505439ca818a881864f5f9cd
Krish C Naik <krishnaik06@gmail.com>
2021-06-14 20:46:40 +0530

    Add files via upload

---
+++ Tutorial 7/app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,Response
+import cv2
+
+app=Flask(__name__)
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
